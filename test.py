216e1f13396d7f15c5a3fe1716159eee765d531e
eloso85 <eloso85@gmail.com>
2023-10-25 13:48:30 -0500

    added test

---
+++ test.py
@@ -0,0 +1,35 @@
+# name = "Beau"
+# age = 39
+
+# print(type(name)== str)
+# print(isinstance(name, str))
+
+# age = 2.9
+# print(isinstance(age, float))
+
+# age = int("20")
+# print(isinstance(age, int))
+
+# number = "20"
+# age = int(number)
+# print(isinstance(age,int))
+
+# 1 + 1
+# 2 - 1
+# 2 * 2
+# 4 / 2
+# 4 % 3
+# 4 ** 2
+# 5 // 2
+
+# age = 8 
+# age += 8
+# print(age)
+
+a = 1
+b = 2
+
+a == b #false
+a != b #true
+
+
