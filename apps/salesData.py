772c09b6eb2fe0601f42208a19f8d0da9be8e3b4
eloso85 <eloso85@gmail.com>
2023-11-27 22:48:34 -0600

    adding sales data

--- apps/salesData.py
+++ apps/salesData.py
@@ -5,3 +5,31 @@ def read_sales_data(filename):
             product, amount = line.strip().split(': ')
             sales[product] = sales.get(product, 0) + int(amount)
     return sales
+
+
+def total_sales(sales):
+    return sum(sales.values())
+
+
+def average_sales(sales):
+    return total_sales(sales) / len(sales) if sales else 0
+
+
+def best_selling_product(sales):
+    return max(sales, key=sales.get)
+
+
+def display_sales_summary(sales):
+    print(f"Total Sales: {total_sales(sales)}")
+    print(f"Average Sales per Product: {average_sales(sales)}")
+    print(f"Best Selling Product: {best_selling_product(sales)}")
+
+
+def main():
+    sales_data_file = 'data/sales_data.txt'
+    sales = read_sales_data(sales_data_file)
+    display_sales_summary(sales)
+
+
+if __name__ == "__main__":
+    main()
